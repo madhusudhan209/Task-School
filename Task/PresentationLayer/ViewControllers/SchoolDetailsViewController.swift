9f6e46337606357e0f8da9e8a13f6ab0c85ff1b2
Madhusudhan P <msreddy.p209@gmail.com>
2019-11-18 22:23:45 +0530

    Kept default segue code in School Details ViewController

--- Task/PresentationLayer/ViewControllers/SchoolDetailsViewController.swift
+++ Task/PresentationLayer/ViewControllers/SchoolDetailsViewController.swift
@@ -90,6 +90,18 @@ class SchoolDetailsViewController: UIViewController {
     @IBAction func callTapped(_ sender: UIControl) {
         SharedManager.shared.call((schoolObj.phone)!)
     }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 extension String {
